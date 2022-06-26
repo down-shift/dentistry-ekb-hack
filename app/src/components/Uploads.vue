caeab03d60144f6fad30739a783d1b381d061d32
AlanTheKnight <51376126+AlanTheKnight@users.noreply.github.com>
2022-06-26 09:29:26 +0300

    Diagnosis

--- app/src/components/Uploads.vue
+++ app/src/components/Uploads.vue
@@ -36,18 +36,39 @@ onUnmounted(() => {
       </div>
     </div>
     <div v-else>
-      <div v-if="history && history.length" v-for="req in history">
-        <div class="mt-3 row" v-if="req.image !== null">
+      <div v-if="history && history.length" v-for="(obj, i) in history">
+        <div class="mt-3 row" v-if="obj.image !== null">
           <div
             class="col col-auto d-flex justify-content-center align-items-center"
           >
-            <img :src="req.image" class="analysed_image" />
+            <img :src="obj.image" class="analysed_image mb-3" />
           </div>
           <div class="col-auto">
-            <div><b>Файл: </b>{{ req.filename }}</div>
-            <b>Обнаружено кариесов:</b> {{ req.result.boxes.length }}
+            <div><b>Файл: </b>{{ obj.filename }}</div>
+            <b>Обнаружено кариесов:</b> {{ obj.result.boxes.length }}
+
+            <div
+              v-if="obj.result.boxes.length === 0"
+              class="alert alert-success mt-3"
+            >
+              <i class="bi-check-lg me-2"></i>
+              Кариесы не обнаружены. Обращайтесь к стоматологу раз в полгода.
+            </div>
+            <div
+              v-else-if="obj.result.boxes.length <= 3"
+              class="alert alert-warning mt-3"
+            >
+              <i class="bi-exclamation-lg me-2"></i>
+              Зубы нуждаются в дополнительном уходе. Посещайте стоматолога раз в
+              месяц.
+            </div>
+            <div v-else class="alert alert-danger mt-3">
+              <i class="bi-x-lg me-2"></i>
+              Зубы в плохом состоянии. Необходимо срочное посещение стоматолога.
+            </div>
           </div>
         </div>
+        <hr v-if="i !== history.length - 1" />
       </div>
       <div v-else-if="history">
         <div class="text-center">Вы еще не загрузили ни одного файла</div>
@@ -55,3 +76,11 @@ onUnmounted(() => {
     </div>
   </div>
 </template>
+
+<style>
+.analysed_image {
+  object-fit: scale-down;
+  height: 100px;
+  width: 100px;
+}
+</style>
